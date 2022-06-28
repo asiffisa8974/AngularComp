f055eebf911f587fff9a120e6139370314d84629
asiffisa8974 <100788635+asiffisa8974@users.noreply.github.com>
2022-06-28 17:50:51 +0530

    Add files via upload

---
+++ app.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+import { NewCompComponent } from './new-comp/new-comp.component';
+import { NewComp2Component } from './new-comp2/new-comp2.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    NewCompComponent,
+    NewComp2Component
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
